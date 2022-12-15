645f93ba2779c31ef20c4f781b77dad31b9625c6
Nidasakinaa <114220455+Nidasakinaa@users.noreply.github.com>
2022-12-15 22:24:37 +0700

    Add files via upload

---
+++ PHP Praktikum/if.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>if</title>
+</head>
+<body>
+    <h2>Penggunaan kontrol if</h2>
+    <?php
+    $umur=15;
+    echo("Umur anda $umur tahun <br>");
+    if($umur>=17)
+    {
+        echo("Anda boleh masuk");
+    }else
+    {
+        echo("Anda tidak boleh masuk");
+    }
+    ?>
+</body>
+</html>
